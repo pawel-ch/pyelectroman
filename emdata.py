pawel-ch/pyelectroman

commit 9c53731d1ca13aa3c8a4a97d70b106bdc6c46721

Add files via upload
Poprawiłem kod do wersji Python 3.11, debuger nie znajduje błędów, jednak nie pojawia się ekran z grą. Konsola wyświetla komunikat "Process finished with exit code 0".

emdata.py
@@ -85,7 +85,8 @@ class SpriteSet:
         self.index = 0
         return self
 
-    def next(self):
+    @property
+    def __next__(self):
         if self.index == 32:
             raise StopIteration
         while self.sprites[self.index] is None:
@@ -100,7 +101,7 @@ class SpriteSet:
         set_file_path = os.path.join(set_file_path, set_name)
         set_file_path += ".ebs"
         jfile = open(set_file_path, "rt")
-        self.set = json.load(jfile, encoding='ascii')
+        self.set = json.load(jfile)
         for spr in range(64):
             if self.is_used(spr):
                 sprite = SpriteData()
@@ -145,7 +146,9 @@ class LevelData:
         level_file_path = os.path.join(gl.data_folder, filename)
         level_file_path += ".ebl"
         jfile = open(level_file_path, "rt")
-        self.data = json.load(jfile, encoding='ascii')
+        json_data = jfile.read()  # odczytaj zawartość pliku jako string
+        jfile.close()  # zamknij plik
+        self.data = json.loads(json_data)  # użyj json.loads z odczytanymi danymi
 
 
 #noinspection PyArgumentEqualDefault
@@ -303,8 +306,20 @@ class Level(LevelData):
 
     def __init_enemy(self, sidx, position):
         sprite = self.get_sprite(sidx)
-        num = (sprite.param & 0x7F) / 3
-        anims, frames = gl.enemies.get_anims(num)
+        if sprite is None or sprite.param is None:
+            # Handle the case when sprite or sprite.param is None
+            # Set appropriate default values or raise an exception
+            num = 0
+        else:
+            num = (sprite.param & 0x7F) / 3
+
+        if gl.enemies is not None and hasattr(gl.enemies, "get_anims"):
+            anims, frames = gl.enemies.get_anims(num)
+        else:
+            # Handle the case when gl.enemies is None or doesn't have the get_anims attribute
+            # Set appropriate default values for anims and frames or raise an exception
+            anims = []
+            frames = []
         if num == 2:  # enemy types can be hardcoded
             entity = ga.EnemyFlying([sprite], position)
         else:
@@ -326,7 +341,7 @@ class Level(LevelData):
         LevelData.load(self, name)
         set1_name = self.data["names"][0]
         set2_name = self.data["names"][1]
-        assert set1_name is not "" and set2_name is not ""
+        assert set1_name != "" and set2_name != ""
         self.set1.load(set1_name)
         self.set2.load(set2_name)
         cntr = 0
@@ -368,10 +383,16 @@ class Level(LevelData):
                 entity = self.__get_active_entity(entity[1], entity[2])
             entity.set_origin(screen)  # remember screen for deletion
             screen.active.append(entity)
-            if isinstance(entity, ga.Checkpoint) and param == 1:
-                # active checkpoint - level start
+
+        entity = 42
+        if isinstance(entity, ga.Checkpoint) and param == 1:
+            # active checkpoint - level start
+            if self.name in gl.level_names:
                 level_number = gl.level_names.index(self.name)
                 gl.checkpoint.update(level_number, screen_number, position)
+            else:
+                # Handle the case when self.name is not found in gl.level_names
+                print("Error: Level name not found in gl.level_names")
         else:
             entity = ga.Entity([sprite], position)
             screen.background.append(entity)
@@ -428,7 +449,7 @@ def main():
     game.init()
     for l in range(8):
         lname = gl.level_names[l]
-        print "Level:", lname
+        print ("Level:"), lname
         level = Level()
         level.load(lname)
         for s in range(256):
@@ -442,10 +463,10 @@ def main():
                     else:
                         actives[name] = 1
                 if actives:
-                    print "Screen %3d: " % s,
+                    print("Screen %3d: " % s)
                     for key, value in actives.items():
-                        print "%s(%d) " % (key, value),
-                    print
+                        print("%s(%d) " % (key, value), end="")
+                    print()
     game.quit()
 
 if __name__ == "__main__":

em.py
@@ -10,7 +10,7 @@ import emother as ot
 import pygame
 import logging
 import time
-
+            
 
 class Gameplay:
     """
@@ -50,52 +50,49 @@ class Gameplay:
                              pygame.K_0: self.on_k_0}
         self.deferred = None
 
-    def init_map(self):
+    @staticmethod
+    def init_map():
         """Initialize level map"""
         # pylint: disable-msg=E1121
         screens_map = pygame.Surface((32, 32))
         pixels = pygame.PixelArray(screens_map)
         # pylint: enable-msg=E1121
-        FULL = 0x33AA33
-        EMPTY = 0x333333
+        full = 0x33AA33
+        empty = 0x333333
         screens = gl.screen_manager.get_screens()
         for scr in range(256):
             if screens[scr]:
-                pixels[(scr % 16) * 2 + 0][(scr / 16) * 2 + 0] = FULL
-                pixels[(scr % 16) * 2 + 1][(scr / 16) * 2 + 0] = FULL
-                pixels[(scr % 16) * 2 + 0][(scr / 16) * 2 + 1] = FULL
-                pixels[(scr % 16) * 2 + 1][(scr / 16) * 2 + 1] = FULL
+                pixels[(scr % 16) * 2 + 0][(scr // 16) * 2 + 0] = full
+                pixels[(scr % 16) * 2 + 1][(scr // 16) * 2 + 0] = full
+                pixels[(scr % 16) * 2 + 0][(scr // 16) * 2 + 1] = full
+                pixels[(scr % 16) * 2 + 1][(scr // 16) * 2 + 1] = full
             else:
-                pixels[(scr % 16) * 2 + 0][(scr / 16) * 2 + 0] = EMPTY
-                pixels[(scr % 16) * 2 + 1][(scr / 16) * 2 + 0] = EMPTY
-                pixels[(scr % 16) * 2 + 0][(scr / 16) * 2 + 1] = EMPTY
-                pixels[(scr % 16) * 2 + 1][(scr / 16) * 2 + 1] = EMPTY
+                pixels[(scr % 16) * 2 + 0][(scr // 16) * 2 + 0] = empty
+                pixels[(scr % 16) * 2 + 1][(scr // 16) * 2 + 0] = empty
+                pixels[(scr % 16) * 2 + 0][(scr // 16) * 2 + 1] = empty
+                pixels[(scr % 16) * 2 + 1][(scr // 16) * 2 + 1] = empty
         del pixels
         return screens_map
 
     def show_map(self, pos):
         """Display level map (for debug only)"""
         scr = gl.screen_manager.get_screen_number()
-        CURRENT = 0xFFFFFF
+        current = 0xFFFFFF
         # pylint: disable-msg=E1121
         screens_map_copy = pygame.Surface((32, 32))
         screens_map_copy.blit(self.screens_map, (0, 0))
         pixels = pygame.PixelArray(screens_map_copy)
         # pylint: enable-msg=E1121
-        pixels[(scr % 16) * 2 + 0][(scr / 16) * 2 + 0] = CURRENT
-        pixels[(scr % 16) * 2 + 1][(scr / 16) * 2 + 0] = CURRENT
-        pixels[(scr % 16) * 2 + 0][(scr / 16) * 2 + 1] = CURRENT
-        pixels[(scr % 16) * 2 + 1][(scr / 16) * 2 + 1] = CURRENT
+        pixels[(scr % 16) * 2 + 0][(scr // 16) * 2 + 0] = current
+        pixels[(scr % 16) * 2 + 1][(scr // 16) * 2 + 0] = current
+        pixels[(scr % 16) * 2 + 0][(scr // 16) * 2 + 1] = current
+        pixels[(scr % 16) * 2 + 1][(scr // 16) * 2 + 1] = current
         del pixels
         gl.window.blit(screens_map_copy, pos)
 
-    def show_info(self):
-        """Display status line"""
-        di.status_line.show()
-
     def display_screen(self, screen):
         """Display all objects (active and background) on the screen"""
-        gl.screen_manager.update_active() # make sure newly created objects get displayed
+        gl.screen_manager.update_active()  # make sure newly created objects get displayed
         if screen:
             for entity in screen.background:
                 entity.display()
@@ -113,15 +110,18 @@ class Gameplay:
         for deferred in self.deferred:
             deferred()
 
-    def display_hero(self):
+    @staticmethod
+    def display_hero():
         """Display player's character"""
         gl.player.display()
 
-    def display_indicators(self):
+    @staticmethod
+    def display_indicators():
         """Display weapon indicators (and other icons)"""
         di.indicators.display()
 
-    def move_player(self, offset):
+    @staticmethod
+    def move_player(offset):
         position = gl.player.get_position() + offset
         gl.player.set_position(position)
 
@@ -161,7 +161,8 @@ class Gameplay:
             if gl.player.get_y() <= ((gl.SCREEN_Y + 2) * gl.SPRITE_Y):
                 self.move_player((0, gl.SPRITE_Y))
 
-    def on_k_tab(self):
+    @staticmethod
+    def on_k_tab():
         gl.show_collisions = False if gl.show_collisions else True
 
     def on_k_1(self):
@@ -211,11 +212,13 @@ class Gameplay:
         gl.current_level = 7
         self.load_level()
 
-    def on_k_0(self):
+    @staticmethod
+    def on_k_0():
         if pygame.key.get_mods() & pygame.KMOD_SHIFT:
             gl.player.select_weapon(0)
 
-    def on_k_escape(self):
+    @staticmethod
+    def on_k_escape():
         gl.loop_main_loop = False
 
     def load_level(self):
@@ -224,26 +227,28 @@ class Gameplay:
         self.screens_map = self.init_map()
         start_screen = gl.checkpoint.get_screen()
         start_position = gl.checkpoint.get_position()
-        gl.disks = 0 # no disks collected after level load
+        gl.disks = 0  # no disks collected after level load
         if start_position:
-            start_position += XY(gl.SPRITE_X / 2, gl.SPRITE_Y)
+            start_position += XY(gl.SPRITE_X // 2, gl.SPRITE_Y)
             gl.screen_manager.change_screen(start_screen)
             gl.player.stand(start_position)
 
-    def loop_begin(self):
+    @staticmethod
+    def loop_begin():
         di.clear_screen()
 
     def loop_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 gl.loop_main_loop = False
-        keys = pygame.key.get_pressed()
-        for key, state in enumerate(keys):
-            if (key in self.key_handlers) and state:
-                self.key_handlers[key]()
+        pressed_keys = pygame.key.get_pressed()
+        for key, state in self.key_handlers.items():
+            if pressed_keys[key]:
+                state()
         self.controller.update()
 
-    def loop_run(self):
+    @staticmethod
+    def loop_run():
         gl.screen = gl.screen_manager.get_screen()
         if gl.screen:
             for active in gl.screen.active:
@@ -258,9 +263,10 @@ class Gameplay:
         self.show_map((640 - 32 - 8, 8))
         self.show_info()
 
-    def show(self):
+    @staticmethod
+    def show():
         """Display the screen."""
-        di.message(XY(500, 8),"logic: {0:>4.1f}\nrender: {1:>4.1f}".format(
+        di.message(XY(500, 8), "logic: {0:>4.1f}\nrender: {1:>4.1f}".format(
             round(gl.logic_time * 1000, 1), round(gl.render_time * 1000, 1)))
         di.show()
 
@@ -272,23 +278,28 @@ class Gameplay:
         clock = pygame.time.Clock()
         while gl.loop_main_loop:
             # logic processing starts here
-            logic_start = time.clock()
+            logic_start = time.time()
             self.loop_begin()
             self.loop_events()
             self.loop_run()
-            gl.logic_time = time.clock() - logic_start
+            gl.logic_time = time.time() - logic_start
             # logic processing ended
             # rendering starts here
-            render_start = time.clock()
+            render_start = time.time()
             self.loop_end()
-            gl.render_time = time.clock() - render_start
+            gl.render_time = time.time() - render_start
             # rendering ended
-            self.show() # show the screen
+            self.show()  # show the screen
             gl.counter += 1
             clock.tick(20)  # keep constant frame rate (20fps)
 
     def stop(self):
         pass
+
+
+def show_info():
+    """Display status line"""
+    di.status_line.show()
 
 
 class Game:
@@ -302,31 +313,35 @@ class Game:
         else:
             logging.basicConfig(filename=gl.log_filename,
                                 filemode="w", format=(
-                                '%(levelname)s: %(funcName)s(): %(message)s'),
+                                    '%(levelname)s: %(funcName)s(): %(message)s'),
                                 level=logging.DEBUG)
 
-    def init(self):
+    @staticmethod
+    def init():
         di.init_display()
         di.info_lines.add("pyelectroman started")
-        time.clock()
 
-    def quit(self):
+    @staticmethod
+    def quit():
         di.quit_display()
 
-def fast_main():
-    game = Game()
-    game.init()
-    gameplay = Gameplay()
-    gameplay.start()
-    gameplay.run()
-    gameplay.stop()
-    game.quit()
+    @staticmethod
+    def fast_main():
+        game = Game()
+        game.init()
+        gameplay = Gameplay()
+        gameplay.start()
+        gameplay.run()
+        gameplay.stop()
+        game.quit()
 
 
 def profile_main():
     # This is the main function for profiling
     # We've renamed our original main() above to real_main()
-    import cProfile, pstats, StringIO
+    import cProfile
+    import pstats
+    import StringIO
     prof = cProfile.Profile()
     prof = prof.runctx("real_main()", globals(), locals())
     stream = StringIO.StringIO()
@@ -334,12 +349,11 @@ def profile_main():
     stats.sort_stats("calls")
     stats.print_stats(80)  # how many to print
     # The rest is optional.
-    #stats.print_callees()
-    #stats.print_callers()
+    # stats.print_callees()
+    # stats.print_callers()
     logging.info("Profile data:\n%s", stream.getvalue())
 
+    main = fast_main()
 
-main = fast_main
-
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()

emdisplay.py
@@ -73,15 +73,15 @@ class InfoLines:
         if len(self.lines) == self.max_lines:
             self.lines.pop(0)
         self.lines.append(text)
-        self.update = time.clock()
+        self.update = time.time()
 
     def show(self):
         pos = self.position.copy()
         for line in self.lines:
             pos = message(pos, line)
-        if (time.clock() - self.update > self.max_time) and self.lines:
+        if (time.time() - self.update > self.max_time) and self.lines:
             self.lines.pop(0)
-            self.update = time.clock()
+            self.update = time.time()
 
 info_lines = InfoLines(XY(180, 8), 5, 5) #default info lines buffer
 
@@ -121,7 +121,7 @@ class LEDBar:
         self.value = value % 7
 
     def display(self):
-        #self.value = int(time.clock()) % 7
+        #self.value = int(time.time()) % 7
         position = XY.from_self(self.position)
         for led in range(6):
             gl.display.blit(self.leds[self.mapping[self.value][led]], position)

emgame.py
@@ -2,9 +2,10 @@
 
 import emglobals as gl
 import emdata as da
-from emglobals import XY
 import pygame
 import copy
+
+from emglobals import XY
 
 
 class Controller:
@@ -281,12 +282,12 @@ class Entity:
         if abs(ox) < abs(oy):
             swap_xy = True
             oy, ox = ox, oy
-        sy = (float(oy) / abs(ox)) * 2
+        sy = (float(oy) // abs(ox)) * 2
         fy = 0.01
         # pylint: disable-msg=W0612
         # mutilsampling check with step 2 pixels
-        for step in range(abs(ox) / 2):
-            nx += 2 * cmp(ox, 0)
+        for step in range(abs(ox) // 2):
+            nx += 2 * ((ox > 0) - (ox < 0))
             fy += sy
             ny = int(fy) & ~0x1
             if swap_xy:
@@ -304,20 +305,20 @@ class Entity:
         """Set initial delay depending on mode or object's position."""
         pos = self.get_position()
         if mode == 0:
-            self.delay = ((pos.x / gl.SPRITE_X)
-                          + (pos.y / gl.SPRITE_Y)) % (param + 1)
+            self.delay = ((pos.x // gl.SPRITE_X)
+                          + (pos.y // gl.SPRITE_Y)) % (param + 1)
         elif mode == 1:
-            self.delay = (pos.x / gl.SPRITE_X) % (param + 1)
+            self.delay = (pos.x // gl.SPRITE_X) % (param + 1)
         elif mode == 2:
-            self.delay = (pos.y / gl.SPRITE_Y) % (param + 1)
+            self.delay = (pos.y // gl.SPRITE_Y) % (param + 1)
         elif mode == 3:
             self.delay = 0
         elif mode == 4:
             self.delay = gl.random(param + 1)
         elif mode == 5:
-            self.delay = gl.screen_randoms[pos.x / gl.SPRITE_X] % (param + 1)
+            self.delay = gl.screen_randoms[pos.x // gl.SPRITE_X] % (param + 1)
         elif mode == 6:
-            self.delay = gl.screen_randoms[pos.y / gl.SPRITE_Y] % (param + 1)
+            self.delay = gl.screen_randoms[pos.y // gl.SPRITE_Y] % (param + 1)
         elif mode == 7:
             self.frame = gl.random(len(self.sprites))
             self.delay = 0

emglobals.py
@@ -187,7 +187,7 @@ def rand():
 
 def random(num):
     """ #define random(num)(int)(((long)rand()*(num))/(RAND_MAX+1)) """
-    return int((rand() * num) / 0x8000)
+    return int((rand() * num) // 0x8000)
 
 # -----------------------------------------------------------------------------
 # test code below
@@ -196,17 +196,17 @@ def random(num):
 def main():
     srand(0)
     for i in range(16):
-        print rand()
+        print(rand())
     init_screen_randoms(0)
-    print screen_randoms
+    print(screen_randoms)
     init_screen_randoms(1)
-    print screen_randoms
+    print(screen_randoms)
     init_screen_randoms(2)
-    print screen_randoms
+    print(screen_randoms)
     init_screen_randoms(18)
-    print screen_randoms
+    print(screen_randoms)
     init_screen_randoms(34)
-    print screen_randoms
+    print(screen_randoms)
 
 if __name__ == "__main__":
     main()

emhero.py
@@ -17,12 +17,12 @@ FALL_STEPS = [2, 4, 6, 8, 10, 12, 16, 18, 20, 22, 24,
               26, 28, 32, 34, 36, 38, 40, 42, 44, 48]
 
 # touch procedure names
-touch_procs = {0 : "none", 1 : "battery", 2 : "teleport", 3 :"checkpoint",
-               4 : "killer", 5 : "floppy", 6 :  "exit",
-               7 : "special good", 8 : "special bad"}
+touch_procs = {0: "none", 1: "battery", 2: "teleport", 3: "checkpoint",
+               4: "killer", 5: "floppy", 6: "exit",
+               7: "special good", 8: "special bad"}
 
 
-#noinspection PySimplifyBooleanCheck,PyArgumentEqualDefault
+# noinspection PySimplifyBooleanCheck,PyArgumentEqualDefault
 class PlayerEntity(ga.FSM, ga.Entity):
     """
     Player's entity class, extends ga.Entity.
@@ -31,6 +31,7 @@ class PlayerEntity(ga.FSM, ga.Entity):
     Requires supplying ga.Controller class for player's input.
     Singleton by design.
     """
+
     def __init__(self, controller):
         ga.Entity.__init__(self, [da.EmptySprite()], XY(0, 0))
         ga.FSM.__init__(self)
@@ -86,7 +87,7 @@ class PlayerEntity(ga.FSM, ga.Entity):
         self.frames["RJUMP"] = len(self.sprites["RJUMP"])
         # entering teleport (reverse for leaving)
         self.sprites["TELE"] = [(36, 42), (37, 43), (38, 44),
-                                 (39, 45), (40, 46), (41, 47)]
+                                (39, 45), (40, 46), (41, 47)]
         self.frames["TELE"] = len(self.sprites["TELE"])
         self.switch_state(self.state_init)
         self.keys = 0  # keys needed to open the exit
@@ -94,8 +95,8 @@ class PlayerEntity(ga.FSM, ga.Entity):
         self.temp = 0  # weapons's temperature increase
         self.fired = False  # to disable repeated shots
         self.ammo = 0  # current ammo left
-        self.magazine = [0, 20, 15, 25, 10, 15] # # of shots for each weapon level
-        self.heat = [0,  1,  2,  1,  4,  3] # temperature increase for each level
+        self.magazine = [0, 20, 15, 25, 10, 15]  # # of shots for each weapon level
+        self.heat = [0, 1, 2, 1, 4, 3]  # temperature increase for each level
         self.cooldown = 0  # cooldown counter
 
     def display(self):
@@ -176,7 +177,7 @@ class PlayerEntity(ga.FSM, ga.Entity):
         # then horizontal movement
         side = XY(self.move_vector.x, 0)
         pside, touched = self.check_move(side, self.screen)
-        self.touched.extend(touched) # touch will be handled later
+        self.touched.extend(touched)  # touch will be handled later
         pos = self.get_position()
         pos += pside
         self.set_position(pos)
@@ -242,7 +243,6 @@ class PlayerEntity(ga.FSM, ga.Entity):
                 self.anim = ("LSTAND", "RSTAND")[self.orientation]
                 self.frame = 0
                 self.move_vector.x = 0
-
 
     def state_turn(self, init=False):
         """Handle turning state."""
@@ -511,22 +511,23 @@ class PlayerEntity(ga.FSM, ga.Entity):
         di.status_line.add(" | ammo: %d" % self.ammo)
 
     projectiles = {
-        "1_L" : { "offset" : XY(-38, 32), "step" : -28 },
-        "1_R" : { "offset" : XY(38, 32), "step" : 28 },
-        "2_L" : { "offset" : XY(-46, 32), "step" : -32 },
-        "2_R" : { "offset" : XY(46, 32), "step" : 32 },
-        "3_L" : { "offset" : XY(-46, 32), "step" : -28 },
-        "3_R" : { "offset" : XY(46, 32), "step" : 28 },
-        "4_L" : { "offset" : XY(-46, 32), "step" : -24 },
-        "4_R" : { "offset" : XY(46, 32), "step" : 24 },
-        "5_L" : { "offset" : XY(-36, 8), "step" : -32 },
-        "5_R" : { "offset" : XY(36, 8), "step" : 32 },
+        "1_L": {"offset": XY(-38, 32), "step": -28},
+        "1_R": {"offset": XY(38, 32), "step": 28},
+        "2_L": {"offset": XY(-46, 32), "step": -32},
+        "2_R": {"offset": XY(46, 32), "step": 32},
+        "3_L": {"offset": XY(-46, 32), "step": -28},
+        "3_R": {"offset": XY(46, 32), "step": 28},
+        "4_L": {"offset": XY(-46, 32), "step": -24},
+        "4_R": {"offset": XY(46, 32), "step": 24},
+        "5_L": {"offset": XY(-36, 8), "step": -32},
+        "5_R": {"offset": XY(36, 8), "step": 32},
     }
 
     def fire_weapon(self):
         """
         Fire the weapon!
         """
+
         def add_projectile(type, pos=None):
             if not pos:
                 ppos = self.get_position().copy() + self.projectiles[type]["offset"]
@@ -552,7 +553,7 @@ class PlayerEntity(ga.FSM, ga.Entity):
                     self.select_weapon(self.power - 1)
 
 
-#noinspection PyArgumentEqualDefault
+# noinspection PyArgumentEqualDefault
 class Projectile(ga.Entity):
     def __init__(self, type):
         ga.Entity.__init__(self, [da.EmptySprite()], XY(0, 0))
@@ -576,12 +577,14 @@ class Projectile(ga.Entity):
             sprite = self.sprites[1]
             gl.display.blit(sprite.image, self.get_position() + XY(0, gl.SPRITE_Y))
 
+
 # -----------------------------------------------------------------------------
 # test code below
 
 
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()

emother.py
@@ -49,8 +49,8 @@ class Enemies:
         self.enemy[3].frames["SRIGHT"] = len(self.enemy[3].anims["SRIGHT"])
 
     def get_anims(self, enemy_num):
-        return (self.enemy[enemy_num].anims,
-                self.enemy[enemy_num].frames)
+        enemy_index = int(enemy_num)
+        return (self.enemy[enemy_index].anims, self.enemy[enemy_index].frames)
 
 
 class WeaponData:

sprite_info.py
@@ -34,7 +34,7 @@ class SpriteData():
 def load_data(filename):
     global sprites
     global areas
-    set_path = os.path.join(r"D:\Work\PyEM\misc\conversion\newdata", filename)
+    set_path = os.path.join(r"C:\Users\Grzegorz\Downloads\pyelectroman-master\data", filename)
     params_path = os.path.join(set_path, filename + ".ebs")
     f = open(params_path, "rt")
     data = json.load(f)

commit 132e9cd3e0d319f90b093be6813320ffda03548a

Fix errors in level loading introduced in 9c53731d

emdata.py
@@ -316,20 +316,8 @@ class Level(LevelData):
 
     def __init_enemy(self, sidx, position):
         sprite = self.get_sprite(sidx)
-        if sprite is None or sprite.param is None:
-            # Handle the case when sprite or sprite.param is None
-            # Set appropriate default values or raise an exception
-            num = 0
-        else:
-            num = (sprite.param & 0x7F) / 3
-
-        if gl.enemies is not None and hasattr(gl.enemies, "get_anims"):
-            anims, frames = gl.enemies.get_anims(num)
-        else:
-            # Handle the case when gl.enemies is None or doesn't have the get_anims attribute
-            # Set appropriate default values for anims and frames or raise an exception
-            anims = []
-            frames = []
+        num = (sprite.param & 0x7F) / 3
+        anims, frames = gl.enemies.get_anims(num)
         if num == 2:  # enemy types can be hardcoded
             entity = ga.EnemyFlying([sprite], position)
         else:
@@ -379,8 +367,8 @@ class Level(LevelData):
         action = sprite.action
         param = sprite.param
         position = XY(x * gl.SPRITE_X, y * gl.SPRITE_Y)
+        entity = ga.Entity([sprite], position)
         if (flags == 0x80) & (action == 0):
-            entity = ga.Entity([sprite], position)
             screen.collisions.append(entity)
         elif flags & 0x80:
             entity = self.__get_active_entity(sidx, position)
@@ -393,15 +381,10 @@ class Level(LevelData):
             entity.set_origin(screen)  # remember screen for deletion
             screen.active.append(entity)
 
-        entity = 42
         if isinstance(entity, ga.Checkpoint) and param == 1:
             # active checkpoint - level start
-            if self.name in gl.level_names:
-                level_number = gl.level_names.index(self.name)
-                gl.checkpoint.update(level_number, screen_number, position)
-            else:
-                # Handle the case when self.name is not found in gl.level_names
-                print("Error: Level name not found in gl.level_names")
+            level_number = gl.level_names.index(self.name)
+            gl.checkpoint.update(level_number, screen_number, position)
         else:
             entity = ga.Entity([sprite], position)
             screen.background.append(entity)